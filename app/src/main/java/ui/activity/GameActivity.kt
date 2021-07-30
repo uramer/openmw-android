559d10c71209c4807b1cd7e092991936d776d267
Dmitry <docent27@ukr.net>
2021-07-30 20:07:51 +0300

    Update

--- app/src/main/java/ui/activity/GameActivity.kt
+++ app/src/main/java/ui/activity/GameActivity.kt
@@ -92,6 +92,7 @@ class GameActivity : SDLActivity() {
                 Os.setenv("OSG_VERTEX_BUFFER_HINT", "VBO", true)
                 Os.setenv("LIBGL_FB", "1", true)
                 Os.setenv("LIBGL_USEVBO", "1", true)
+                Os.setenv("LIBGL_NOHIGHP", "1", true)
             } catch (e: ErrnoException) {
                 Log.e("OpenMW", "Failed setting environment variables.")
                 e.printStackTrace()
--- gradlew
+++ gradlew
@@ -5,7 +5,7 @@
 ##  Gradle start up script for UN*X
 ##
 ##############################################################################
-
+cp settings.cfg app/src/main/assets/libopenmw/openmw/settings.cfg
 # Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 DEFAULT_JVM_OPTS=""
 
